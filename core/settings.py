3ffeb5ddf45421a418697d18352f750904bd78b9
atilacapovilla <capovillaatila@gmail.com>
2024-12-02 10:28:02 -0300

    Alterado para usar o sqlit3

--- core/settings.py
+++ core/settings.py
@@ -89,18 +89,18 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'ac_pessoal',
-        'USER': 'postgres',
-        'PASSWORD': 'ncc1701',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql',
+    #     'NAME': 'ac_pessoal',
+    #     'USER': 'postgres',
+    #     'PASSWORD': 'ncc1701',
+    #     'HOST': 'localhost',
+    #     'PORT': '5432',
+    # }
 }
 
 
